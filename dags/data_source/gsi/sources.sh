4cf93d887c466afeda157b9a15a8a24c9d934cb2
Tanakrit Chaukajung <>
2022-02-21 23:44:20 +0700

    feat: add gsi dag

---
+++ dags/data_source/gsi/sources.sh
@@ -0,0 +1,1 @@
+curl -LfO 'https://www.itu.int/dms_pub/itu-d/opb/str/D-STR-GCI.01-2021-PDF-E.pdf'
---
+++ dags/gsi_dag.py
@@ -0,0 +1,158 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from pywebhdfs.webhdfs import PyWebHdfsClient
+from datetime import datetime, timedelta
+from pprint import pprint
+from PyPDF2 import PdfFileReader
+import os
+import json
+import pandas as pd
+import numpy as np
+
+
+def transform():
+    datasource_path = "/opt/airflow/dags/data_source/gsi"
+    output_path = "/opt/airflow/dags/output/gsi"
+
+    def create_security_table(pdf_file, country_file, meta_data_file, page_start, page_end, year):
+        # creating a pdf file object
+        pdfFileObj = open("{}".format(pdf_file), 'rb')
+        # show dataframe about country
+        country_df = pd.read_csv("{}".format(country_file))
+        # creating a pdf reader object
+        pdfReader = PdfFileReader(pdfFileObj)
+        # show official column dataframe
+        gsi_index_meta_data = pd.read_excel("{}".format(
+            meta_data_file), sheet_name='Global Cyber_Metadata', header=1)
+        all_num_lst = []
+        for i in range(page_start - 1, page_end):
+            pageObj = pdfReader.getPage(i)
+            pageTxt = pageObj.extractText()
+            pageTxt = pageTxt.replace('\n', '')
+            Txtlst = pageTxt.split(' ')
+            s = 'Source:'
+            lst = []
+            for i in range(len(Txtlst)):
+                if (Txtlst[i] == s):
+                    lst.append(i)
+            lst1 = []
+            lst2 = []
+            count = 0
+            for i in lst:
+                for j in range(i - 6, i):
+                    count += 1
+                    if count <= 6:
+                        lst1.append(Txtlst[j])
+                    else:
+                        lst2.append(Txtlst[j])
+            all_num_lst.append(lst1)
+            all_num_lst.append(lst2)
+            number_df = pd.DataFrame(all_num_lst)
+            number_df = number_df.dropna()
+            number_df = number_df.reset_index(drop=True)
+            number_df = number_df.rename(columns={0: 'Index score', 1: 'Legal Measures', 2: 'Technical Measures',
+                                                  3: 'Organizational Measures', 4: 'Capacity Building', 5: 'Cooperation'})
+            early_gsi_df = pd.concat([country_df, number_df], axis=1)
+            early_gsi_df = early_gsi_df.melt(
+                id_vars=['Country'], var_name="index_group", value_name="value")
+            gsi_df = early_gsi_df.merge(
+                gsi_index_meta_data, how='left', left_on='index_group', right_on='Name')
+            # add column of year is 2020
+            ingest_date = datetime.now()
+            gsi_df['year'] = year
+            gsi_df['value'] = gsi_df['value'].astype(float)
+            gsi_df["value"] = np.where(gsi_df["pillar"].isna(
+            ), gsi_df['value'] / 100, gsi_df['value'] / 20)
+            drop_col = ['index_group', 'Code', 'Type',
+                        'Name', 'Unit', 'Definition', 'Source']
+            gsi_df = gsi_df.drop(drop_col, axis=1)
+            gsi_df.columns = gsi_df.columns.str.lower()
+            gsi_df['index'] = 'Global Cybersecurity Index'
+            gsi_df['master_index'] = 'Global Cybersecurity Index'
+            gsi_df['organizer'] = 'ITU'
+            gsi_df['unit_2'] = 'Score'
+            gsi_df['ingest_date'] = ingest_date.strftime("%Y-%m-%d %H:%M:%S")
+
+            gsi_df.to_csv('{}/GSI_{}_{}.csv'.format(output_path, year,
+                                                    ingest_date.strftime("%Y%m%d%H%M%S")), index=False)
+
+    def ingestion_init():
+        # Get config
+        json_file = open('{}/config.json'.format(datasource_path))
+        conf_main = json.load(json_file)
+        json_file.close()
+
+        for config in conf_main:
+            create_security_table(
+                pdf_file=config['pdf_file'],
+                country_file=config['country_file'],
+                meta_data_file=config['meta_data_file'],
+                page_start=config['page_start'],
+                page_end=config['page_end'],
+                year=config['year'],
+            )
+
+    ingestion_init()
+
+
+default_args = {
+    'owner': 'ETDA',
+    'depends_on_past': False,
+    'start_date': '2021-01-25',
+    'email': ['oulespaul@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retry_delay': timedelta(minutes=5),
+    'schedule_interval': '@yearly',
+}
+
+dag = DAG('GSI', default_args=default_args, catchup=False)
+
+
+def store_to_hdfs():
+    hdfs = PyWebHdfsClient(host='10.121.101.130',
+                           port='50070', user_name='hdfs')
+    my_dir = '/raw/index_dashboard/Global/GSI'
+    hdfs.make_dir(my_dir)
+    hdfs.make_dir(my_dir, permission=755)
+
+    path = "/opt/airflow/dags/output/gsi"
+
+    os.chdir(path)
+
+    for file in os.listdir():
+        if file.endswith(".csv"):
+            file_path = f"{path}/{file}"
+
+            with open(file_path, 'r', encoding="utf8") as file_data:
+                my_data = file_data.read()
+                hdfs.create_file(
+                    my_dir+f"/{file}", my_data.encode('utf-8'), overwrite=True)
+
+                pprint("Stored! file: {}".format(file))
+                pprint(hdfs.list_dir(my_dir))
+
+
+with dag:
+    ingestion = BashOperator(
+        task_id='ingestion',
+        bash_command='cd /opt/airflow/dags/data_source/gsi && ./sources.sh ',
+    )
+
+    transform = PythonOperator(
+        task_id='transform',
+        python_callable=transform,
+    )
+
+    load_to_hdfs = PythonOperator(
+        task_id='load_to_hdfs',
+        python_callable=store_to_hdfs,
+    )
+
+    clean_up_output = BashOperator(
+        task_id='clean_up_output',
+        bash_command='rm -f /opt/airflow/dags/output/gsi/*',
+    )
+
+ingestion >> transform >> load_to_hdfs >> clean_up_output
