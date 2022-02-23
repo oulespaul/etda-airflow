e86620414ba0017ebb6792d1653d850adb784e27
Tanakrit Chaukajung <>
2022-02-23 11:02:04 +0700

    fix: change idi

--- dags/idi_dag.py
+++ dags/idi_dag.py
@@ -6,74 +6,99 @@ from pywebhdfs.webhdfs import PyWebHdfsClient
 from pprint import pprint
 import pandas as pd
 import os
+from decimal import Decimal
+import json
 
 
 def transform():
-    data_source_dir = '/opt/airflow/dags/data_source/idi'
+    datasource_path = "/opt/airflow/dags/data_source/idi"
+    output_path = "/opt/airflow/dags/output/idi"
 
-    df = pd.read_excel(
-        f'{data_source_dir}/Global Ranking Dashboard.xlsx', sheet_name='IDI', skiprows=1)
+    def ingestion_n_transform(source_file_name, series_dict):
+        df = pd.read_excel(
+            f'{datasource_path}/{source_file_name}', sheet_name='IDI', skiprows=1, engine="openpyxl")
 
-    i = 3
-    df = pd.concat([df.iloc[:, :i],
-                    pd.DataFrame('',
-                                 columns=['sub_index', 'pillar', 'sub_pillar', 'sub_sub_pillar', 'indicator',
-                                          'sub_indicator', 'others'],
-                                 index=df.index), df.iloc[:, i:]],
-                   axis=1)
+        i = 3
+        df = pd.concat([df.iloc[:, :i],
+                        pd.DataFrame('',
+                                     columns=['sub_index', 'pillar', 'sub_pillar', 'sub_sub_pillar', 'indicator',
+                                              'sub_indicator', 'others'],
+                                     index=df.index), df.iloc[:, i:]],
+                       axis=1)
 
-    def get_value_from_key(key, sub_key):
-        return series_dict.get(key, {}).get(sub_key, "")
+        def get_value_from_key(key, sub_key):
+            return series_dict.get(key, {}).get(sub_key, "")
 
-    df["index"] = df["Index Name"].apply(get_value_from_key, args=("index",))
-    df["pillar"] = df["Index Name"].apply(get_value_from_key, args=("pillar",))
-    df["indicator"] = df["Index Name"].apply(
-        get_value_from_key, args=("indicator",))
-    df["others"] = df["Index Name"].apply(get_value_from_key, args=("others",))
+        df["index"] = df["Index Name"].apply(
+            get_value_from_key, args=("index",))
+        df["pillar"] = df["Index Name"].apply(
+            get_value_from_key, args=("pillar",))
+        df["indicator"] = df["Index Name"].apply(
+            get_value_from_key, args=("indicator",))
+        df["others"] = df["Index Name"].apply(
+            get_value_from_key, args=("others",))
 
-    df.drop(['Type', 'Index Name', 'Unit 1'], axis=1, inplace=True)
-    df.rename(columns={'Index': 'master_index', 'ผู้จัดทำ': 'organizer',
-              'Year': 'year', 'Unit 2': 'unit_2'}, inplace=True)
+        df.drop(['Type', 'Index Name', 'Unit 1'], axis=1, inplace=True)
+        df.rename(columns={'Index': 'master_index', 'ผู้จัดทำ': 'organizer',
+                           'Year': 'year', 'Unit 2': 'unit_2'}, inplace=True)
 
-    df = df.melt(
-        id_vars=['master_index', 'organizer', 'year', 'sub_index', 'pillar', 'sub_pillar',
-                 'sub_sub_pillar', 'indicator', 'sub_indicator', 'others', 'unit_2'],
-        var_name="country",
-        value_name="value"
-    )
+        df = df.melt(
+            id_vars=['master_index', 'organizer', 'year', 'sub_index', 'pillar', 'sub_pillar',
+                     'sub_sub_pillar', 'indicator', 'sub_indicator', 'others', 'unit_2'],
+            var_name="country",
+            value_name="value"
+        )
 
-    def is_number(value):
-        try:
-            Decimal(value)
-            return True
-        except Exception:
-            return False
+        def is_number(value):
+            try:
+                Decimal(value)
+                return True
+            except Exception:
+                return False
 
-    ingest_date = datetime.now()
+        ingest_date = datetime.now()
 
-    df['index'] = "ICT Development Index"
-    df['ingest_date'] = ingest_date.strftime("%Y/%m/%d %H:%M")
-    df['unit_2'].replace(['Score', 'Value'], ['Rank', 'Score'], inplace=True)
+        df['index'] = "ICT Development Index"
+        df['ingest_date'] = ingest_date.strftime("%Y/%m/%d %H:%M")
+        df['unit_2'].replace(['Score', 'Value'], [
+                             'Rank', 'Score'], inplace=True)
 
-    # Filter value
-    df = df[df['value'].apply(is_number)]
-    df = df[~df['value'].isnull()]
+        # Filter value
+        df = df[df['value'].apply(is_number)]
+        df = df[~df['value'].isnull()]
 
-    col = ["country", "year", "master_index", "organizer", "index", "sub_index", "pillar", "sub_pillar", "sub_sub_pillar",
-           "indicator", "sub_indicator", "others", "unit_2", "value", "ingest_date"]
+        col = ["country", "year", "master_index", "organizer", "index", "sub_index", "pillar", "sub_pillar", "sub_sub_pillar",
+               "indicator", "sub_indicator", "others", "unit_2", "value", "ingest_date"]
 
-    df = df[col]
-    df = df.sort_values(by=['country', 'year', 'pillar', 'indicator'])
+        df = df[col]
+        df = df.sort_values(by=['country', 'year', 'pillar', 'indicator'])
 
-    year_list = df['year'].unique()
+        year_list = df['year'].unique()
 
-    for year in year_list:
-        final = df[df['year'] == year].copy()
-        current_year = str(year)[0:4]
-        final['year'] = current_year
+        for year in year_list:
+            final = df[df['year'] == year].copy()
+            current_year = str(year)[0:4]
+            final['year'] = current_year
 
-        final.to_csv('/opt/airflow/dags/output/idi/IDI_{}_{}.csv'.format(
-            current_year, ingest_date.strftime("%Y%m%d%H%M%S")), index=False)
+            final.to_csv('{}/IDI_{}_{}.csv'.format(
+                output_path,
+                current_year, 
+                ingest_date.strftime("%Y%m%d%H%M%S")), 
+                index=False)
+
+    def ingestion_init():
+        # Get config
+        json_file = open('{}/config.json'.format(datasource_path))
+        conf_main = json.load(json_file)
+        json_file.close()
+
+        for config in conf_main:
+            ingestion_n_transform(
+                source_file_name=config['source_file_name'],
+                series_dict=config['series_dict'],
+            )
+
+    ingestion_init()
 
 
 default_args = {
@@ -89,6 +114,18 @@ default_args = {
 
 dag = DAG('idi', default_args=default_args, catchup=False)
 
+def ingest_data():
+    hdfs = PyWebHdfsClient(host='10.121.101.130',
+                           port='50070', user_name='hdfs')
+    source_file_byte = '/raw/index_dashboard/File_Upload/Global Ranking Dashboard.xlsx'
+
+    data_source = hdfs.read_file(source_file_byte)
+
+    with open('/opt/airflow/dags/data_source/idi/Global Ranking Dashboard.xlsx', 'wb') as file_out:
+        file_out.write(data_source)
+        file_out.close()
+
+    pprint("Ingested!")
 
 def store_to_hdfs():
     hdfs = PyWebHdfsClient(host='10.121.101.130',
@@ -115,9 +152,9 @@ def store_to_hdfs():
 
 
 with dag:
-    ingestion = BashOperator(
+    ingestion = PythonOperator(
         task_id='ingestion',
-        bash_command='cd /opt/airflow/dags/data_source/idi && ./sources.sh ',
+        python_callable=ingest_data,
     )
 
     transform = PythonOperator(
