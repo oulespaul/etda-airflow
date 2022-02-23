a01b39d71ff80d5caa50b47e822a8056a6b95b68
Tanakrit Chaukajung <>
2022-02-23 10:00:48 +0700

    fix: gsi task ingest

--- dags/data_source/gsi/sources.sh
+++ dags/data_source/gsi/sources.sh
@@ -1,1 +1,1 @@
-curl -LfO 'https://www.itu.int/dms_pub/itu-d/opb/str/D-STR-GCI.01-2021-PDF-E.pdf'
+wget -O D-STR-GCI.01-2021-PDF-E.pdf https://www.itu.int/dms_pub/itu-d/opb/str/D-STR-GCI.01-2021-PDF-E.pdf
